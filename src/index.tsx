28bcee21a2f96277ae13a2671d6423e98450c476
ymSophia <ym960928@sina.com>
2021-11-19 23:38:50 +0800

    暂存，有问题

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (): React.ReactElement => {
+	return <div>Hello</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
